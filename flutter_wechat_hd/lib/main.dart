d5f7092fec8d354d6338cbb640ce4cfeb02ef62d
dealon <wjcwsq@163.com>
2019-07-23 15:48:51 +0800

    movie 开始学习

--- flutter_wechat_hd/lib/main.dart
+++ flutter_wechat_hd/lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_wechat_hd/test_dart.dart';
+import 'package:flutter_wechat_hd/test_func_dart.dart';
+import 'dart:ui';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+  Map<String, WidgetBuilder> _routes = {
+    Navigator.defaultRouteName: (context)=>MyHomePage(title: 'title',),
+  };
+
+
   @override
   Widget build(BuildContext context) {
+    //test();
 
-    test();
 
     return MaterialApp(
       title: 'Flutter Demo',
@@ -24,7 +31,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: _routes,
     );
   }
 }
@@ -52,12 +60,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+
       _counter++;
+    });
+  }
+
+  void _decrementCounter(){
+    setState((){
+      _counter--;
     });
   }
 
@@ -79,20 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -105,10 +102,21 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
+          SizedBox(width: 10,),
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Descrement',
+            child: Icon(Icons.remove),
+          ),
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
